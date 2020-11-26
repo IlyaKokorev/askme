d25cbcd2b42d9f85862d3efa57ebe7f3684e00ef
IlyaKokorev <kockorev.ilya@gmail.com>
2020-11-26 13:56:05 +0300

    added JS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def index
+    @users = [
+        User.new(
+            id: 1,
+            name: 'Ilya',
+            username: 'ilyacatran',
+            avatar_url: 'https://linlt.com/upload/photos/2019/01/udthvlXOe2d3xWv8vLk4_22_f268a0a090e3cefcb0564430f6bd3ca8_avatar.jpg?cache=0'
+        ),
+        User.new(
+            id: 2,
+            name: 'User2',
+            username: 'user22'
+        )
+    ]
   end
 
   def new
@@ -11,7 +24,7 @@ class UsersController < ApplicationController
   def show
     @user = User.new(
         name: 'Ilya',
-        username: 'IlyaCatran',
+        username: 'ilyacatran',
         avatar_url: 'https://linlt.com/upload/photos/2019/01/udthvlXOe2d3xWv8vLk4_22_f268a0a090e3cefcb0564430f6bd3ca8_avatar.jpg?cache=0'
     )
 
